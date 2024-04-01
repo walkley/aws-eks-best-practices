2c7a733b9100f06d675dfc0cab7214346cf9ebff
Walkley He <walkleyhe@gmail.com>
2024-04-01 22:33:23 +0800

    update translation tool

--- translate_tools/translate_cn.py
+++ translate_tools/translate_cn.py
@@ -1,13 +1,9 @@
-
-# traverse the directory and sub-directories recursively, 
-# use python script to translate the markdown file, excluding the file name with postfix of *.ko.md
-# save the translated file to the same directory
+import glob
 import json
 import os
 import re
 import sys
 import boto3
-import marko
 
 bedrock = boto3.client(
   service_name='bedrock-runtime', 
@@ -16,50 +12,12 @@ bedrock = boto3.client(
 
 modelId_Claude3_Sonnet = 'anthropic.claude-3-sonnet-20240229-v1:0'
 modelId_Claude3_Haiku = "anthropic.claude-3-haiku-20240307-v1:0"
-chinese_comma_pattern = r'([\u4e00-\u9fa5]+[\w ]*),'
-
-# split the long text to chunks, split by paragraphs, each chunk should less than 2048 characters
-def split_text(text, chunk_max_length):
-    chunks = []
-    current_chunk = ''
-    for paragraph in text.split('\n\n'):
-        if len(current_chunk) + len(paragraph) + 1 > chunk_max_length:
-            chunks.append(current_chunk)
-            current_chunk = ''
-        current_chunk += paragraph + '\n\n'
-    chunks.append(current_chunk)
-    return chunks
-
-markdown_file = '''# Expenditure awareness
-
-Expenditure awareness is understanding who, where and what is causing expenditures in your EKS cluster. Getting an accurate picture of this data will help raise awareness of your spend and highlight areas to remediate.
-
-
-## Recommendations
-### Use Cost Explorer
-
-[AWS Cost Explorer](https://aws.amazon.com/aws-cost-management/aws-cost-explorer/) has an easy-to-use interface that lets you visualize, understand, and manage your AWS costs and usage over time. You can analyze cost and usage data, at various levels using the filters available in Cost Explorer.
-
-#### EKS Control Plane and EKS Fargate costs
-
-Using the filters, we can query the costs incurred for the EKS costs at the Control Plane and Fargate Pod as shown in the diagram below:
-
-![Cost Explorer - EKS Control Plane](../images/eks-controlplane-costexplorer.png)
-
-Using the filters, we can query the aggregate costs incurred for the Fargate Pods across regions in EKS - which includes both vCPU-Hours per CPU and GB Hrs as shown in the diagram below:
-
-![Cost Explorer - EKS Fargate](../images/eks-fargate-costexplorer.png)
-
-#### Tagging of Resources
-
-Amazon EKS supports [adding AWS tags](https://docs.aws.amazon.com/eks/latest/userguide/eks-using-tags.html) to your Amazon EKS clusters. This makes it easy to control access to the EKS API for managing your clusters. Tags added to an EKS cluster are specific to the AWS EKS cluster resource, they do not propagate to other AWS resources used by the cluster such as EC2 instances or load balancers. Today, cluster tagging is supported for all new and existing EKS clusters via the AWS API, Console, and SDKs.
-
-AWS Fargate is a technology that provides on-demand, right-sized compute capacity for containers. Before you can schedule pods on Fargate in your cluster, you must define at least one Fargate profile that specifies which pods should use Fargate when they are launched. 
-
-'''
+chinese_comma_pattern = r'([\u4e00-\u9fa5]+[\w )]*),'
+chinese_colon_pattern = r'([\u4e00-\u9fa5]+[\w )]*):'
+chunk_max_length = 2048
 
 system_prompt = '''
-你是一位AWS资深解决方案架构师，同时精通英文和中文。你正在协助用户将Markdown格式的英文技术文档翻译成简体中文，请在协助翻译时严格遵守以下规则：
+你是一位AWS资深解决方案架构师，同时精通英文和中文，也非常熟悉Markdown文件的格式。你正在协助用户将Markdown格式的英文技术文档翻译成简体中文，请在协助翻译时严格遵守以下规则：
 - 翻译后的中文格式需精确对应原 Markdown 文件，包含标题、代码块、列表等。
 - 必须保证中文翻译的自然、精确以及流畅。
 - 对于代码块、json、XML、HTML 及其他只为计算机展示或执行的内容，这类内容不要翻译，原样复制过来即可。
@@ -76,6 +34,24 @@ user_prompt = '''
 请仔细阅读并识别上面的Markdown内容的格式和结构，按照系统提示词中的规则思考哪些内容需要翻译，哪些内容不需要翻译。
 完成翻译后，请将翻译后的内容放在 <translated_markdown></translated_markdown> 中，不要输出其它与翻译无关的内容。
 '''
+
+# split the long text to chunks, split by paragraphs, each chunk should less than 2048 characters
+def split_text(text, chunk_max_length = chunk_max_length):
+    '''
+    Splits a given text into chunks that do not exceed a specified maximum length.
+    This function takes a string `text` and an integer `chunk_max_length` as inputs. 
+    Each chunk attempts to contain as many complete paragraphs as possible without exceeding the `chunk_max_length`. 
+    '''
+
+    chunks = []
+    current_chunk = ''
+    for paragraph in text.split('\n\n'):
+        if len(current_chunk) + len(paragraph) + 1 > chunk_max_length:
+            chunks.append(current_chunk)
+            current_chunk = ''
+        current_chunk += paragraph + '\n\n'
+    chunks.append(current_chunk)
+    return chunks
 
 def translate_chunk(markdown_content):
     # don't translate if the markdown content is empty(spaces or tabs, etc)
@@ -111,15 +87,22 @@ def translate_chunk(markdown_content):
     translated_content = translated_content.strip(" \n")
     # replace half-width punctuation marks with full-width punctuation marks, such as: ", ' etc.
     translated_content = re.sub(chinese_comma_pattern, r'\1，', translated_content)
+    translated_content = re.sub(chinese_colon_pattern, r'\1：', translated_content)
     return translated_content
 
-# translate the whole markdown file, split the file with chunks, translate each chunk, finally concat all translated chunks
 def translate_file(markdown_file):
+    """
+    Translates the content of a markdown file into Chinese and saves the translated content into a new file.
+    This function reads the content of the specified markdown file, splits the content into manageable chunks, 
+        translates each chunk individually to avoid exceeding API limits or encountering memory issues, 
+        and then concatenates all translated chunks. The translated content is saved in a new file within the same directory as the original file, 
+        with the new file name having a '.zh.md' extension to indicate the translation language.
+    """
     with open(markdown_file, 'r', encoding='utf-8') as f:
         content = f.read()
 
     # split the content into chunks
-    chunks = split_text(content, 2048)
+    chunks = split_text(content)
 
     # translate each chunk
     translated_chunks = []
@@ -135,22 +118,24 @@ def translate_file(markdown_file):
     # concat all translated chunks
     translated_content = '\n\n'.join(translated_chunks)
 
-    # save the translated content to the same directory, file name format as *.cn.md
-    translated_file = os.path.splitext(markdown_file)[0] + '.cn.md'
+    # save the translated content to the same directory, file name format as *.zh.md
+    translated_file = os.path.splitext(markdown_file)[0] + '.zh.md'
     with open(translated_file, 'w', encoding='utf-8') as f:
         f.write(translated_content)
 
-    return ""
-
 def translate_directory(path):
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            if file.endswith('.md') and not file.endswith('.ko.md') and not file.endswith('.cn.md'):
-                translate_file(os.path.join(root, file))
-                # print(f"translating: {os.path.join(root, file)}")
+    if os.path.isfile(path):
+        translate_file(path)
+        # print(f"translating {path}")
+    elif os.path.isdir(path):
+        for file_path in glob.glob(f"{path}/**/*.md", recursive=True):
+            if not file_path.endswith(('.ko.md', '.zh.md')):
+                translate_file(file_path)
+                # print(f"translating {file_path}")
+    else:
+        print(f"Invalid path: {path}")
 
 if __name__ == '__main__':
-    # if there's parameter, use it as the path to translate, otherwise use current directory as the path to translate
     path = os.getcwd()
     if len(sys.argv) > 1:
         path = sys.argv[1]
