cc5f8fd8adb7ec4d79fb8ddb7bd3e8f0f7063a12
Walkley He <walkleyhe@gmail.com>
2024-04-02 21:42:53 +0800

    update translate tool, use prefilled assistant content to translate long file without split and translate seperated chunks

--- translate_tools/translate_cn.py
+++ translate_tools/translate_cn.py
@@ -1,13 +1,26 @@
 import glob
 import json
+import logging
 import os
 import re
 import sys
+import time
 import boto3
+from botocore.client import Config as BotoConfig
 
+logger = logging.getLogger(__name__)
+logging.StreamHandler(sys.stdout)
+h1 = logging.StreamHandler(sys.stdout)
+h1.setLevel(logging.DEBUG)
+logger.addHandler(h1)
+logger.setLevel(logging.DEBUG)
+
+TIMEOUT = 300
+config = BotoConfig(read_timeout=TIMEOUT, connect_timeout=TIMEOUT, retries={"max_attempts": 3})
 bedrock = boto3.client(
   service_name='bedrock-runtime', 
-  region_name="us-west-2"
+  region_name="us-west-2",
+  config = config
 )
 
 modelId_Claude3_Sonnet = 'anthropic.claude-3-sonnet-20240229-v1:0'
@@ -28,45 +41,25 @@ system_prompt = '''
 '''
 
 user_prompt = '''
-下面<markdown>中的内容从原始Markdown文件中按段落分拆后需要翻译的Markdown内容片段：
+下面<markdown>中的内容从原始Markdown文件中读取的内容：
 <markdown>{{markdown_content}}</markdown>
 
 请仔细阅读并识别上面的Markdown内容的格式和结构，按照系统提示词中的规则思考哪些内容需要翻译，哪些内容不需要翻译。
 完成翻译后，请将翻译后的内容放在 <translated_markdown></translated_markdown> 中，不要输出其它与翻译无关的内容。
 '''
 
-# split the long text to chunks, split by paragraphs, each chunk should less than 2048 characters
-def split_text(text, chunk_max_length = chunk_max_length):
+def complete_translation(system_prompt: str, user_content: str, translated_content: str, modelId=modelId_Claude3_Haiku):
     '''
-    Splits a given text into chunks that do not exceed a specified maximum length.
-    This function takes a string `text` and an integer `chunk_max_length` as inputs. 
-    Each chunk attempts to contain as many complete paragraphs as possible without exceeding the `chunk_max_length`. 
+    Complete translation by sending the already translated part to prefill the assistant, assistant should continue to translate the remaining content.
     '''
-
-    chunks = []
-    current_chunk = ''
-    for paragraph in text.split('\n\n'):
-        if len(current_chunk) + len(paragraph) + 1 > chunk_max_length:
-            chunks.append(current_chunk)
-            current_chunk = ''
-        current_chunk += paragraph + '\n\n'
-    chunks.append(current_chunk)
-    return chunks
-
-def translate_chunk(markdown_content):
-    # don't translate if the markdown content is empty(spaces or tabs, etc)
-    if not markdown_content.strip(" \t\n"):
-        return markdown_content
-
-    uer_content = user_prompt.replace("{{markdown_content}}", markdown_content)
     messages = [
     {
         "role": "user",
-        "content": uer_content
+        "content": user_content
     },
     {
         "role": "assistant",
-        "content": "<translated_markdown>"
+        "content": translated_content.rstrip() #final assistant content cannot end with trailing whitespace
     }]
 
     body = json.dumps({
@@ -76,67 +69,65 @@ def translate_chunk(markdown_content):
         "system": system_prompt,
         "messages":  messages
     })
-
-    response = bedrock.invoke_model(body=body, modelId=modelId_Claude3_Sonnet)
+    response = bedrock.invoke_model(body=body, modelId=modelId)
     response_body = json.loads(response.get('body').read())
-    translated_content = ""
-    for line in response_body['content']:
-        translated_content += line['text']
 
-    translated_content = translated_content.rstrip("</translated_markdown>")
-    translated_content = translated_content.strip(" \n")
-    # replace half-width punctuation marks with full-width punctuation marks, such as: ", ' etc.
-    translated_content = re.sub(chinese_comma_pattern, r'\1，', translated_content)
-    translated_content = re.sub(chinese_colon_pattern, r'\1：', translated_content)
-    return translated_content
-
+    return response_body
+    
 def translate_file(markdown_file):
-    """
-    Translates the content of a markdown file into Chinese and saves the translated content into a new file.
-    This function reads the content of the specified markdown file, splits the content into manageable chunks, 
-        translates each chunk individually to avoid exceeding API limits or encountering memory issues, 
-        and then concatenates all translated chunks. The translated content is saved in a new file within the same directory as the original file, 
-        with the new file name having a '.zh.md' extension to indicate the translation language.
-    """
+    # check file size > 100k
+    if os.path.getsize(markdown_file) > 100000:
+        logger.error(f"{markdown_file} is larger than 100k which cannot be handled")
+        return
+
+    start_time = time.time()
+    logger.info(f"translating {markdown_file} ...")
     with open(markdown_file, 'r', encoding='utf-8') as f:
-        content = f.read()
+        markdown_content = f.read()
 
-    # split the content into chunks
-    chunks = split_text(content)
+    uer_content = user_prompt.replace("{{markdown_content}}", markdown_content)
+    translated_content = "<translated_markdown>"
+    total_input_tokens = 0
+    total_output_tokens = 0
+    while True:
+        response_body = complete_translation(system_prompt, uer_content, translated_content)
+        for line in response_body['content']:
+            translated_content += line['text']
+        
+        input_tokens = response_body.get('usage').get('input_tokens')
+        output_tokens = response_body.get('usage').get('output_tokens')
+        total_input_tokens += input_tokens
+        total_output_tokens += output_tokens
+        logger.info(f"    completed one trunk, input_tokens: {input_tokens}, output_tokens: {output_tokens}")
+        # break the loop(stop translation) if stop_reason is "end_turn" or translated_content contains "</translated_markdown>"
+        if response_body.get('stop_reason') == "end_turn" or "</translated_markdown>" in translated_content:
+            break
+    
+    translated_content = translated_content.strip()
+    if translated_content.startswith("<translated_markdown>") and translated_content.endswith("</translated_markdown>"):
+        translated_content = translated_content[len("<translated_markdown>"):-len("</translated_markdown>")].strip()
+        # replace half-width punctuation marks with full-width punctuation marks, such as: ", ' etc.
+        translated_content = re.sub(chinese_comma_pattern, r'\1，', translated_content)
+        translated_content = re.sub(chinese_colon_pattern, r'\1：', translated_content)
+        translated_file = os.path.splitext(markdown_file)[0] + '.zh.md'
+        with open(translated_file, 'w', encoding='utf-8') as f:
+            f.write(translated_content)
+        logger.info(f"done! [{int(time.time() - start_time)}s], consumed tokens: input = {total_input_tokens}, output = {total_output_tokens}")
+    else:
+        logger.error(f"Error: {markdown_file} translation failed")
 
-    # translate each chunk
-    translated_chunks = []
-    for index, chunk in enumerate(chunks):
-        percent = round(100*(index+1)/len(chunks))
-        sys.stdout.write(f"\rtranslating: {markdown_file} [{percent}%]")
-        sys.stdout.flush()
-        translated_chunks.append(translate_chunk(chunk))
-
-    sys.stdout.write(f"\rtranslating: {markdown_file} [done!]\n")
-    sys.stdout.flush()
-
-    # concat all translated chunks
-    translated_content = '\n\n'.join(translated_chunks)
-
-    # save the translated content to the same directory, file name format as *.zh.md
-    translated_file = os.path.splitext(markdown_file)[0] + '.zh.md'
-    with open(translated_file, 'w', encoding='utf-8') as f:
-        f.write(translated_content)
-
-def translate_directory(path):
+def translate(path):
     if os.path.isfile(path):
         translate_file(path)
-        # print(f"translating {path}")
     elif os.path.isdir(path):
         for file_path in glob.glob(f"{path}/**/*.md", recursive=True):
             if not file_path.endswith(('.ko.md', '.zh.md')):
                 translate_file(file_path)
-                # print(f"translating {file_path}")
     else:
-        print(f"Invalid path: {path}")
+        logger.error(f"Invalid path: {path}")
 
 if __name__ == '__main__':
     path = os.getcwd()
     if len(sys.argv) > 1:
         path = sys.argv[1]
-    translate_directory(path)
+    translate(path)
